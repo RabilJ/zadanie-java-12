7cd3901895d2a6c52d6474640d05be5aa6a0e220
RabilJ <jakub.popielarski7@gmail.com>
2018-10-18 17:26:08 +0200

    zadanie 12.1

---
+++ src/task1/CountNumber.java
@@ -0,0 +1,35 @@
+package task1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CountNumber {
+    public static void main(String[] args)throws IOException {
+        File file = new File("numbers.txt");
+        file.createNewFile();
+        BufferedReader bfr = new BufferedReader(new FileReader(file));
+
+        String line = "";
+        List<Integer>numbery = new ArrayList<>();
+        while((line=bfr.readLine())!=null){
+            String []values = line.split("\n");
+            for (int i = 0; i < values.length; i++) {
+                numbery.add(Integer.parseInt(values[i]));
+            }
+        }
+        Collections.sort(numbery);
+        System.out.println(numbery+" ");
+        int freq = 0;
+        for (int i = 0; i < numbery.size(); i++) {
+            if(i<10) {
+                freq = Collections.frequency(numbery, i);
+                System.out.println("Liczba: "+i+" występuje "+ freq+" razy.");
+            }
+        }
+    }
+}
